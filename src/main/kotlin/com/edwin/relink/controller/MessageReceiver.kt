5a58a4c9635f5913ad29a9fbcb93ce2ebf6d2ba4
E-D-W-I-N <scvaersoft503@gmail.com>
2020-12-17 23:39:33 +0700

    rabbitMQ integration

---
+++ src/main/kotlin/com/edwin/relink/controller/MessageReceiver.kt
@@ -0,0 +1,13 @@
+package com.edwin.relink.controller
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+
+
+@Component
+class MessageReceiver {
+    @RabbitListener(queues = ["PogChamp"])
+    fun consumeMessageFromQueue(message: String) {
+        println("Message recieved : $message")
+    }
+}
